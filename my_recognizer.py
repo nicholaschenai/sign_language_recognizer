109c3caa2e06e301670b579f650940409f22c4ba
Nicholas Chen <nicholaschenofficial@gmail.com>
2017-08-27 09:17:40 -0400

    27aug2017 fin all parts

--- my_recognizer.py
+++ my_recognizer.py
@@ -20,6 +20,17 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    # DONE: implement the recognizer
+    for X, lengths in test_set.get_all_Xlengths().values():
+        probability_dict = {}
+        for word, model in models.items():
+            try:
+                probability_dict[word] = model.score(X, lengths)
+            except:
+                probability_dict[word] = float('-inf')
+        probabilities.append(probability_dict)
+        # get the word with the highest logL
+        guesses.append(max(probability_dict.keys(), key=(lambda k: probability_dict[k])))
+            
+    return (probabilities, guesses)    
+    
